013d128e2b625e1cdc00535260b00560d1968c46
HiImFLUSH <92924024+HiImFLUSH@users.noreply.github.com>
2024-12-08 16:27:09 +0000

    Refino1

--- app_ofc/app/app.py
+++ app_ofc/app/app.py
@@ -27,7 +27,7 @@ def index():
 
 @APP.route('/ClinicStats/')
 def listar_clinicstats():
-    # Adicione comentários para explicar o propósito da consulta
+    # 
     clinicstats = db.execute('''
         SELECT c.data, i.instituicao, d.nome, c.internamentos, c.diasInternamento, c.ambulatorio, c.obitos
         FROM ClinicStats c
@@ -48,14 +48,41 @@ def regiao():
     return render_template('regiao.html',
                             Regiao=regiao)
 
+@APP.route('/Instituicao/<String:nome>/')
+def instituicao():
+    # Obtém clinicstats por hospital
+    instituicao = db.execute('''
+        SELECT i.nome, COUNT(c.ID) AS n_reports 
+        FROM Instituicao i
+        NATURAL JOIN ClinicStats
+        GROUP BY nome
+    ''').fetchall()
+    return render_template('instituicao.html',
+                            Instituicao=instituicao)
+
 @APP.route('/Diagnostico/')
-def regiao():
+def diagnostico():
     # Quais diagnosticos possuem mais óbitos, internaçoes e ambulatórios
     diagnostico = db.execute('''
-        SELECT ID, nome, sum(obitos) AS Fatalidades, sum(internamentos) AS Internamentos, sum(ambulatorio) AS Ambulatório, sum(diasInternamento) AS diasInternamento 
+        SELECT ID, nome, sum(obitos) AS Obitos, sum(internamentos) AS Internamentos, sum(ambulatorio) AS Ambulatório, sum(diasInternamento) AS diasInternamento 
         FROM Diagnostico 
         NATURAL JOIN ClinicStats 
-        GROUP BY ID ORDER BY Fatalidades DESC, Internamentos, Ambulatório, diasInternamento
+        GROUP BY ID 
+        ORDER BY Obitos DESC, Internamentos, Ambulatório, diasInternamento
     ''').fetchall()
     return render_template('diagnostico.html',
+                            Diagnostico=diagnostico)
+
+@APP.route('/Regiao_Diagnostico/')
+def r_diagnostico():
+    # Quais regiões possuem mais óbitos, internaçoes e ambulatórios
+    r_diagnostico = db.execute('''
+        SELECT r.nome, sum(c.obitos) AS Obitos, sum(c.internamentos) AS Internamentos, sum(c.ambulatorio) AS Ambulatório, sum(c.diasInternamento) AS diasInternamento 
+        FROM Regiao r
+        JOIN Instituicao i ON r.nome = i.regiao
+        JOIN ClinicStats c ON i.nome = c.instituicao
+        GROUP BY r.nome
+        ORDER BY Obitos DESC, Internamentos, Ambulatório, diasInternamento
+    ''').fetchall()
+    return render_template('r_diagnostico.html',
                             Diagnostico=diagnostico)
